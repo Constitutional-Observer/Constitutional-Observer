eac59384b0bbb66d55cb1064a4e0b11e8e4f3861
Adhavan <adhavan.sivaraj16@gmail.com>
2024-04-27 14:10:43 +0530

    Info about the project

--- frontend/src/lib/components/Footer.svelte
+++ frontend/src/lib/components/Footer.svelte
@@ -1,12 +1,18 @@
-<footer class="grid grid-cols-3">
+<footer class="grid grid-cols-3 gap-x-10">
   <div>
-    This project is open source and will be made available publicly in the near
-    future.
-    <br />
-    This project contains data obtained from constitutionofindia.net and is being
-    being used for non-commercial purposes.
+    <p>
+      This project is open source and will be made available publicly in the
+      near future. This project contains data obtained from
+      constitutionofindia.net and sansad.in and is being being used for
+      non-commercial purposes.
+    </p>
   </div>
-  <div></div>
+  <div>
+    <p>
+      Please email at adhavan [dot] sivaraj16 [at] gmail [dot] com with
+      "Constitutional-Observer" as the subject line
+    </p>
+  </div>
   <div>
     This was co-created and inspired during the course of my undergraduate
     thesis project at Srishti Manipal, with the guidance of Abhishek Hazra.
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -5,15 +5,50 @@
 
   import MainSearch from "$lib/components/MainSearch.svelte";
   import Footer from "$lib/components/Footer.svelte";
+  import { onMount } from "svelte";
 
   // for lyrical divs
   let idx1 = 0;
   let idx2 = 0;
+
+  let dialog;
+
   setInterval(() => {
     idx1 = (idx1 + 10) % text1.length;
     idx2 = (idx2 + 10) % text2.length;
   }, 5000);
+
+  onMount(() => {
+    dialog.showModal();
+  });
 </script>
+
+<dialog
+  bind:this={dialog}
+  on:close={() => (dialog = false)}
+  class="bg-primary rounded-xl w-1/4 shadow-2xl backdrop:bg-black/20 backdrop:backdrop-blur-sm"
+>
+  <h1 class="p-5 text-2xl">Disclaimer</h1>
+  <div class="pl-5 pr-10 py-3">
+    Alpha version disclaimer: You have probably received this link for review.
+    As this is a work in progress, please note: layout and interactive features
+    are subject to change. The search results may not be accurate.
+
+    <br /><br />
+    <i
+      >This project is open to critical feedback and suggestions. Please email
+      at adhavan.sivaraj16@gmail.com with "Constitutional-Observer" as the
+      subject line</i
+    >
+
+    <div>
+      <button
+        class="p-3 my-5 bg-primaryLight rounded-lg"
+        on:click={() => dialog.close()}>Close</button
+      >
+    </div>
+  </div>
+</dialog>
 
 <main class="relative">
   <section class="relative">
