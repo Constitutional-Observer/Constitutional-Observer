5eb60d0a00ad3449076bbf1b36012d35ea953ba2
Adhavan <adhavan.sivaraj16@gmail.com>
2024-05-01 10:29:47 +0530

    Duplication error fix attempt

--- frontend/src/routes/ask/+page.svelte
+++ frontend/src/routes/ask/+page.svelte
@@ -7,94 +7,95 @@
 
 <main class="relative">
   <!-- Main content -->
-
-  <div class="md:grid gap-20 md:grid-cols-5 h-screen text-black">
-    <section class="md:relative scroll-container my-10">
-      <div class="md:absolute top-1/3 left-1/7">
-        <h1 class="text-6xl text-bold">
-          {$query}
-        </h1>
-        <form
-          class="opacity-50 hover:opacity-100 transition-all"
-          method="post"
-          action="?/query"
-        >
-          <div class="w-3/4 flex">
-            <input
-              type="text"
-              name="query"
-              class="p-1 mr-2 w-full text-gray-300"
-              bind:value={$query}
-            />
-            <button
-              type="submit"
-              class="bg-primary text-white px-2 py-1 rounded-md">Search</button
-            >
-          </div>
-        </form>
-        <a href="/" class="underline pt-5">Go back</a>
-      </div>
-    </section>
-
-    {#await form.debates}
-      Loading...
-    {:then debates}
-      <section class="scroll-container col-span-1 md:col-span-2">
-        <h2 class="title-bg">Constituent Assembly Debates</h2>
-        <p>The Constituent Assembly met between 1947 and 1949.</p>
-        <Accordion>
-          {#each debates as debate, index (debate)}
-            <AccordionItem class="card">
-              <svelte:fragment slot="lead">
-                {@html debate.speaker_name} on {@html new Date(
-                  debate.date
-                ).toDateString()}
-              </svelte:fragment>
-              <svelte:fragment slot="summary"
-                >{@html debate.content.substring(0, 100)}</svelte:fragment
+  {#key form}
+    <div class="md:grid gap-20 md:grid-cols-5 h-screen text-black">
+      <section class="md:relative scroll-container my-10">
+        <div class="md:absolute top-1/3 left-1/7">
+          <h1 class="text-6xl text-bold">
+            {$query}
+          </h1>
+          <form
+            class="opacity-50 hover:opacity-100 transition-all"
+            method="post"
+            action="?/query"
+          >
+            <div class="w-3/4 flex">
+              <input
+                type="text"
+                name="query"
+                class="p-1 mr-2 w-full text-gray-300"
+                bind:value={$query}
+              />
+              <button
+                type="submit"
+                class="bg-primary text-white px-2 py-1 rounded-md"
+                >Search</button
               >
-              <svelte:fragment slot="content"
-                >{@html debate.content}</svelte:fragment
-              >
-            </AccordionItem>
-          {/each}
-        </Accordion>
+            </div>
+          </form>
+          <a href="/" class="underline pt-5">Go back</a>
+        </div>
       </section>
-    {:catch}
-      <p>Something went wrong</p>
-    {/await}
 
-    {#await form.sabha}
-      Loading...
-    {:then questions}
-      <section class="scroll-container col-span-1 md:col-span-2">
-        <h2 class="title-bg">Lok Sabha Questions</h2>
-        <Accordion>
-          {#each questions as question, index (question)}
-            <AccordionItem class="card">
-              <svelte:fragment slot="lead"
-                >{@html question.Representative} to {@html question[
-                  "Ministry or Category"
-                ]}, about
-                <h4>{@html question.Title}</h4>
-                <!-- {@html sabha.Name} from {@html sabha.Constituency} in  -->
-              </svelte:fragment>
+      {#await form.debates}
+        Loading...
+      {:then debates}
+        <section class="scroll-container col-span-1 md:col-span-2">
+          <h2 class="title-bg">Constituent Assembly Debates</h2>
+          <p>The Constituent Assembly met between 1947 and 1949.</p>
+          <Accordion>
+            {#each debates as debate, index (debate)}
+              <AccordionItem class="card">
+                <svelte:fragment slot="lead">
+                  {@html debate.speaker_name} on {@html new Date(
+                    debate.date
+                  ).toDateString()}
+                </svelte:fragment>
+                <svelte:fragment slot="summary"
+                  >{@html debate.content.substring(0, 100)}</svelte:fragment
+                >
+                <svelte:fragment slot="content"
+                  >{@html debate.content}</svelte:fragment
+                >
+              </AccordionItem>
+            {/each}
+          </Accordion>
+        </section>
+      {:catch}
+        <p>Something went wrong</p>
+      {/await}
 
-              <svelte:fragment slot="summary">
-                {@html question.questionAnswer.substring(0, 100)}
-              </svelte:fragment>
+      {#await form.sabha}
+        Loading...
+      {:then questions}
+        <section class="scroll-container col-span-1 md:col-span-2">
+          <h2 class="title-bg">Lok Sabha Questions</h2>
+          <Accordion>
+            {#each questions as question, index (question)}
+              <AccordionItem class="card">
+                <svelte:fragment slot="lead"
+                  >{@html question.Representative} to {@html question[
+                    "Ministry or Category"
+                  ]}, about
+                  <h4>{@html question.Title}</h4>
+                  <!-- {@html sabha.Name} from {@html sabha.Constituency} in  -->
+                </svelte:fragment>
 
-              <svelte:fragment slot="content">
-                {@html question.questionAnswer}
-              </svelte:fragment>
-            </AccordionItem>
-          {/each}
-        </Accordion>
-      </section>
-    {:catch}
-      <p>Something went wrong</p>
-    {/await}
-    <!-- {#if form?.streamed?.courts?.length > 0}
+                <svelte:fragment slot="summary">
+                  {@html question.questionAnswer.substring(0, 100)}
+                </svelte:fragment>
+
+                <svelte:fragment slot="content">
+                  {@html question.questionAnswer}
+                </svelte:fragment>
+              </AccordionItem>
+            {/each}
+          </Accordion>
+        </section>
+      {:catch}
+        <p>Something went wrong</p>
+      {/await}
+      <!-- {#if form?.streamed?.courts?.length > 0}
       <section class="scroll-container">
         <h2 class="title-bg">SC and HC judgements</h2>
 
@@ -119,7 +120,8 @@
         </Accordion>
       </section>
     {/if} -->
-  </div>
+    </div>
+  {/key}
 </main>
 
 <!-- Main content -->
