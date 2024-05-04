82f2516912cb8dd8844a950df462decaca614f27
Adhavan <adhavan.sivaraj16@gmail.com>
2024-05-04 14:02:46 +0530

    Parent error file

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { page } from "$app/stores";
+  import Footer from "$lib/components/Footer.svelte";
+</script>
+
+<main class="h-[70vh] grid place-content-center md:px-[10%]">
+  <div class="w-2/6">
+    <h1 class="text-3xl">
+      Thank you for visiting the Constitutional Discourses Observer, but the <bold
+        class="font-semibold"
+        >site or this particular page appears to be unavailable</bold
+      >
+    </h1>
+    <h2 class="text-2xl mt-2 p-1 bg-primary rounded">
+      Please go back to the <a class="text-blue-700" href="/">home page</a> and try
+      again.
+    </h2>
+  </div>
+  <code class="preformatter pt-10 w-1/2"
+    >If this error persists, please contact at adhavan [dot] sivaraj16 [at]
+    gmail [dot] com with "Constitutional-Observer-Error" as the subject line. <br
+    />{$page.status}
+    {$page.error.message}</code
+  >
+</main>
+
+<Footer></Footer>
